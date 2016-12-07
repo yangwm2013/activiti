a2ebd1a900eb954440040ff27a59e0fddcdd926f
yangwm <414225550@qq.com>
2016-12-07 16:26:59 +0800

    d7

--- src/com/yangwm/test/Test.java
+++ src/com/yangwm/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("git m1");
 		System.out.println("git m2");
-		System.out.println("git m3_1");
+		System.out.println("git m3");
 		System.out.println("git m4");
 		System.out.println("git m5");
 		System.out.println("git m6");
